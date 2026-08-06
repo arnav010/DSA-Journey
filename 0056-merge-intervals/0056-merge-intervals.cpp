e1638b6e72b2225ac4ed5cc7e8adafecd8e2794d
arnav010 <arnavgupta8550@gmail.com>
2026-08-06 18:09:53 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,22 +8,16 @@ public:
         sort(intervels.begin(),intervels.end());
 
         for(int i=0; i<n; i++) {
-            int start = intervels[i][0];
-            int end  = intervels[i][1];
-            if(!ans.empty() && end<=ans.back()[1]) {
-                continue;
+            if(ans.empty()|| intervels[i][0]>ans.back()[1]) {//either the ans is empty or if the intervel's start element is greater than the ans's end element..........then the intervel will be a new intervel and it needs to be pushed in the answer
+                ans.push_back(intervels[i]);
+            }
+            else { // the intervel's start element lies between the ans's intervel so therefore the max of the intervel's end element and the ans's end element would be taken as answer
+
+            ans.back()[1] = max(intervels[i][1],ans.back()[1]);
+
             }
 
-            for(int j=i+1; j<n; j++) {
-                if(intervels[j][0]<=end) {
-                    end = max(end,intervels[j][1]);
-                }
-                else {
-                    break;
-                }
-            }
-            ans.push_back({start,end});
         }
-        return ans;
+return  ans;
     }
 };
