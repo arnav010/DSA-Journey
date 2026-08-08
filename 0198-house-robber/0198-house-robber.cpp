01fa4a24bfc18051061cb09bd8ed9ad0638018f8
arnav010 <arnavgupta8550@gmail.com>
2026-08-08 18:35:54 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -11,7 +11,19 @@ int f(int ind,vector<int>&nums,vector<int>&dp) {
 }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n+1,-1);
-        return f(n-1,nums,dp);
+        vector<int>dp(n,0);
+        dp[0] = nums[0];
+
+
+        for(int i=1; i<n; i++) {
+            int take = nums[i];
+            if(i>1) take += dp[i-2];
+            int notTake = 0 + dp[i-1];
+
+            dp[i] = max(take,notTake);
+
+        }
+        return dp[n-1];
+
     }
 };
