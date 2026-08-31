d6b2e553b299e8d9e4c832b877684e7418dc6342
arnav010 <arnavgupta8550@gmail.com>
2026-08-31 17:13:49 +0530

    Time: 1 ms (54.12%), Space: 9.4 MB (58.2%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m1[256] = {0};
+        int m2[256] = {0};//mapping the each character of the string s with the corresponding characterr of the string t
+
+
+int n = s.size();
+        for(int i=0; i<n; i++) {
+            if(m1[s[i]]!=m2[t[i]]) return false;
+             
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
+
+        }
+        return true;
+    }
+};
