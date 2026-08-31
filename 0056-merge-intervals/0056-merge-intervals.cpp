4295dbf790bb23d8faca10dd90f777b87feeb94f
arnav010 <arnavgupta8550@gmail.com>
2026-08-31 21:25:04 +0530

    Time: 3 ms (89.01%), Space: 24 MB (21.57%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervels) {
-        int n = intervels.size();
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+            int n = intervals.size();
 
         vector<vector<int>>ans;
 
-        sort(intervels.begin(),intervels.end());
+        sort(intervals.begin(),intervals.end());
 
         for(int i=0; i<n; i++) {
-            if(ans.empty()|| intervels[i][0]>ans.back()[1]) {//either the ans is empty or if the intervel's start element is greater than the ans's end element..........then the intervel will be a new intervel and it needs to be pushed in the answer
-                ans.push_back(intervels[i]);
+            if(ans.empty()||intervals[i][0]>ans.back()[1]) {
+                ans.push_back(intervals[i]);
             }
-            else { // the intervel's start element lies between the ans's intervel so therefore the max of the intervel's end element and the ans's end element would be taken as answer
-
-            ans.back()[1] = max(intervels[i][1],ans.back()[1]);
-
-            }
-
+            else {
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
         }
-return  ans;
+        }
+        return ans;
     }
 };
