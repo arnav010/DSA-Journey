1981315f8387a33a40195a42f71e4df0ba7314bb
arnav010 <arnavgupta8550@gmail.com>
2026-09-13 19:00:21 +0530

    Time: 0 ms (100%), Space: 42.1 MB (14.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
+     int n  = nums.size();
      int cnt = 0;
-     int ele;
+     int ele ;
 
-     for(int i=0; i<nums.size(); i++) {
-        if(cnt==0) {
-            cnt =1;
-            ele = nums[i];
+//first loop for finding which element might be the possible majority element
+     for(int i=0; i<n; i++) {
+        if(cnt==0) {   //new element
+              cnt = 1;
+              ele = nums[i];
         }
-        else if(nums[i]==ele) {
+        else if(nums[i]==ele) { //same element
             cnt++;
         }
-        else cnt--;
-     }
-
-
-     int cnt1 = 0;
-     for(int i=0; i<nums.size();i++){
-        if(nums[i]==ele) {
-            cnt1++;
+        else{
+            cnt--; //different element
         }
      }
-     if(cnt1>=n/2) return ele;
-     return -1;
-    } 
+         
+
+         //second loop to check whether the possible majority element obtained from loop 1 is genuinely a majority element or not
+     int cnt1 = 0;
+
+     for(int i=0; i<n; i++){
+         if(nums[i]==ele) {
+            cnt1++;
+         }
+     }
+
+     if(cnt1>n/2) return ele;
+     else return -1;   //no majority element
+
+
+
+
+    }
 };
